c13de8698bdeb933679ef23a4f28370d2988c81e
MD. Faysal Rahman <faysal35-3119@diu.edu.bd>
2023-08-22 01:17:36 +0600

    Submit

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ encrypt.addEventListener('click', function (e) {
     return // Exit the function
   }
 
-  let result = ""
+  let result = ''
 
   let s = key
-  
+
   for (let i = 0; i < arr.length; i++) {
     let char = arr[i]
     if (char.toUpperCase(arr[i])) {
@@ -36,6 +36,55 @@ encrypt.addEventListener('click', function (e) {
     }
   }
 
+  let outPutForm = document.getElementById('outPutForm')
+
+  outPutForm.classList.remove('d-none')
+
+  let outPut = document.getElementById('outputData')
+
+  outPut.innerHTML = result
+})
+
+let decryptData = document.getElementById('decryptData')
+
+decryptData.addEventListener('click', function (e) {
+  e.preventDefault()
+
+  let cryptoword = document.getElementById('cryptoword').value
+
+  let key = document.getElementById('encKey').value
+  key = parseInt(key)
+
+  let arr = cryptoword.split('')
+  let isValid = arr.every(item => {
+    let charCode = item.charCodeAt(0)
+    return (
+      (charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)
+    )
+  })
+
+  if (!isValid) {
+    alert('Input contains characters outside the range A-Z.')
+    return
+  }
+
+  let result = ''
+
+  let s = key
+
+  for (let i = 0; i < arr.length; i++) {
+    let char = arr[i]
+
+    if (char.toUpperCase(arr[i])) {
+      let val = Math.ceil(Math.abs(char.charCodeAt(0) - key - 65) / 26)
+      console.log(val)
+      let ch = String.fromCharCode((char.charCodeAt(0) - key - 65) + 26*val + 65)
+      result += ch
+    } else {
+      let ch = String.fromCharCode((char.charCodeAt(0) - key - 65 )+ 26*val + 65)
+      result += ch
+    }
+  }
 
   let outPutForm = document.getElementById('outPutForm')
 
